d0bf73f01f7c8bcff6a6011cb64d862c32b56c3c
paulvinzent <manalopv@gmail.com>
2023-11-23 23:15:18 +0800

    admin

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-app>
+      <v-navigation-drawer app v-model="drawer">
+        <v-list>
+          <v-list-item to="/user-management">
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>User Management</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/branch-management">
+            <v-list-item-icon>
+              <v-icon>mdi-domain</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Branch Management</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/inventory">
+            <v-list-item-icon>
+              <v-icon>mdi-archive</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Inventory</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+  
+      <v-app-bar app color="primary">
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>Admin</v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-app-bar>
+    </v-app>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        drawer: false,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add custom styles for the navbar if needed */
+  </style>
+  
---
+++ src/views/AdDash.vue
@@ -0,0 +1,19 @@
+<template>
+    <Sidebar />
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue';
+  
+  // Components
+  import Sidebar from '../components/Sidebar.vue';
+  
+  export default defineComponent({
+    name: 'AdDash',
+  
+    components: {
+      Sidebar,
+    },
+  });
+  </script>
+  
